adca6393d6c4cc566a0745694798127239bc4987
Simon Reinhardt <simon.reinhardt@ur.de>
2023-08-05 15:47:56 +0200

    ground state annealer

--- ground-state-annealer.c
+++ ground-state-annealer.c
@@ -8,48 +8,148 @@
 #include <stdint.h>
 #include <math.h>
 
-#define N_x  200
-#define N_y  200
+#define N_x  100
+#define N_y  100
 #define SCALAR_TYPE float
 #define SINE sinf
+#define COS cosf
+#define PI 3.141592
 
-#define EPS  0.01
+static const SCALAR_TYPE EPS  = 0.4;
+static const SCALAR_TYPE frustration = 0.25* 2 * PI;
+
+
 static SCALAR_TYPE phases[N_x*N_y]; // phi(i,j) = phases(i + N_y * j)
+
+// ^
+// |
+//
+// j (row)
+//    i (column)   - >
+
 
 // static fields are all initialized to zero
 // static SCALAR_TYPE currents_x[(N_x-1)*N_y];
 // static SCALAR_TYPE *currents_y[N_x*(N_y-1)];
 
-
-static int run_steps(int num_steps) {
-  for (int n = 0; n < num_steps; ++n) {
+static int random_init() {
     for (int i = 0; i < N_x; ++i) {
       for (int j = 0; j < N_y; ++j) {
-        SCALAR_TYPE div_I_super = 0;
-        SCALAR_TYPE phase = phases[i + N_y * j];
-        if (i < N_x-1) {
-          div_I_super += SINE(phases[i+1 + N_y*j] - phase);
-        }
-        if (i > 0) {
-          div_I_super += SINE(phases[i-1 + N_y*j] - phase);
-        }
-        if (j < N_y - 1) {
-          div_I_super += SINE(phases[i + N_y*(j+1)] - phase);
-        }
-        if (j > 0) {
-          div_I_super += SINE(phases[i + N_y*(j-1)] - phase);
-        }
-        phases[i + N_y *j] -= EPS * div_I_super;
+        // generate random number in [-pi,pi)
+        phases[i + N_y * j] = 2*PI * ((float) rand() / (float) RAND_MAX) - PI;
+      }
+    }
+}
+
+static int run_step(SCALAR_TYPE temp, SCALAR_TYPE temp_scale_factor) {
+  for (int i = 0; i < N_x; ++i) {
+    for (int j = 0; j < N_y; ++j) {
+      SCALAR_TYPE div_I_super = 0;
+      SCALAR_TYPE phase = phases[i + N_y * j];
+      if (i < N_x-1) {
+        div_I_super += SINE(phases[i+1 + N_y*j] - phase - frustration * j);
+      }
+      if (i > 0) {
+        div_I_super += SINE(phases[i-1 + N_y*j] - phase + frustration *j);
+      }
+      if (j < N_y - 1) {
+        div_I_super += SINE(phases[i + N_y*(j+1)] - phase);
+      }
+      if (j > 0) {
+        div_I_super += SINE(phases[i + N_y*(j-1)] - phase);
+      }
+      phases[i + N_y *j] += EPS * div_I_super;
+      if (temp > 0) {
+        float local_temp = temp *(1 + temp_scale_factor * (abs(i - N_x/2) + abs(j - N_y/2)) / ((float) N_x/2+ (float) N_y/2));
+        //printf("i=%d, j=%d, local_temp = %g\n",i,j, local_temp);
+        phases[i + N_y * j] += 2*local_temp*((float) rand() / (float) RAND_MAX) - local_temp;
       }
     }
   }
 }
 
+static SCALAR_TYPE free_energy() {
+  SCALAR_TYPE f = 0;
+  for (int i = 0; i < N_x; ++i) {
+    for (int j = 0; j < N_y; ++j) {
+      SCALAR_TYPE phase = phases[i + N_y * j];
+      if (i < N_x-1) {
+        f += 1-COS(phases[i+1 + N_y*j] - phase - frustration * j);
+      }
+      if (j < N_y - 1) {
+        f += 1-COS(phases[i + N_y*(j+1)] - phase);
+      }
+    }
+  }
+  return f;
+}
+
+static int save_to_file(char *basename) {
+  char output_filename[300];
+
+
+  //
+  // save phases to basename.dat
+  //
+  
+  sprintf(output_filename, "%s.%s", basename, ".dat");
+  FILE *file = fopen(output_filename, "w");
+  fprintf(file, "#\t\tcolumn(i)\t\trow(j)\t\t\t\tphase\n");
+  for (int j = 0; j < N_y; ++j) {
+    for (int i = 0; i < N_x; ++i) {
+      fprintf(file, "%d\t\t%d\t\t%.5g\n",i,j,phases[i+N_y*j]);
+    }
+    fprintf(file, "\n");
+  }
+  fclose(file);
+  
+  //
+  // save I_x to basename_Ix.dat
+  //
+
+  sprintf(output_filename, "%s_Ix.dat", basename);
+  file = fopen(output_filename, "w");
+  fprintf(file, "#\t\tcolumn(i)\t\trow(j)\t\t\tI_x\n");
+  for (int j = 0; j < N_y; ++j) {
+    for (int i = 0; i < N_x-1; ++i) {
+      SCALAR_TYPE I_x =  SINE(phases[i+1 + N_y*j] - phases[i+N_y*j] - frustration * j);
+      fprintf(file, "%d\t\t%d\t\t%.5g\n",i,j,I_x);
+    }
+    fprintf(file, "\n");
+  }
+  fclose(file);
+  
+  
+  //
+  // save I_y to basename_Ix.dat
+  //
+  sprintf(output_filename, "%s_Iy.dat", basename);
+  file = fopen(output_filename, "w");
+  fprintf(file, "#\t\tcolumn(i)\t\trow(j)\t\t\tI_y\n");
+  for (int j = 0; j < N_y-1; ++j) {
+    for (int i = 0; i < N_x; ++i) {
+      SCALAR_TYPE I_y =  SINE(phases[i + N_y*(j+1)] - phases[i + N_y*j]);
+      fprintf(file, "%d\t\t%d\t\t%.5g\n",i,j,I_y);
+    }
+    fprintf(file, "\n");
+  }
+  fclose(file);
+  
+}
+
 int
 main (int argc, char **argv)
 {
+  random_init();
   printf("Running ground state annealer\n");
   printf("N_x = %d, N_y = %d\n", N_x, N_y);
-  run_steps(100000);
+  float T_start = 1;
+  int num_steps = 200000;
+  for (int i = 0; i < num_steps; ++i) {
+    float temp = T_start * ( 1 - (float) i / num_steps);
+    printf("i = %10d, temp = %10g, f = %.13g\n", i, temp, free_energy());
+    run_step(temp, 1);
+  }  
+  save_to_file("output");
   return 0;
 }
---
+++ plot-file.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3 
+
+import numpy as np
+import sys
+import glob
+import argparse
+import io
+import os.path
+import sys
+import matplotlib.pyplot as plt
+import scipy.signal
+
+
+if np.__version__ < '1.14.1':
+    sys.exit("numpy version " + np.__version__ + " is too old")
+    
+def open_3d_file(file):
+    fh = open(file, 'r')
+    header = fh.readline().rstrip()
+    contents = fh.read().rstrip()
+    
+    list_of_blocks = contents.split("\n\n")
+    num_blocks = len(list_of_blocks)
+    arrays = []
+    for i, block in enumerate(list_of_blocks):
+#        print("reading block %d / %d" % (i, num_blocks))
+        arrays.append(np.genfromtxt(io.StringIO(block)))
+    first_shape = arrays[0].shape
+    for i in range(len(arrays)-1, -1, -1):
+        shape = arrays[i].shape
+        if shape != first_shape:
+            print("block ", i, " with first line", arrays[i][0], " does not match :", shape, " != ", first_shape)
+            del arrays[i]
+    return np.stack(arrays), header
+
+
+def save_3d_file(output_file, data, header):
+    fh = open(output_file, 'w')
+    fh.write(header + "\n")
+    for block in data:
+        np.savetxt(fh, block, fmt="%.17g", delimiter="\t")
+        fh.write("\n")
+    fh.close()
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-o', '--output', help="basename for output data file")
+parser.add_argument('-f', '--force', action="store_true", help="overwrite existing files")
+
+args = parser.parse_args()
+
+data_x, header = open_3d_file('output_Ix.dat')
+data_y, header = open_3d_file('output_Iy.dat')
+I_x = np.swapaxes(data_x, 0, 1)
+I_y = np.swapaxes(data_y, 0, 1)
+
+I_x = I_x[:,:,2]
+I_y = I_y[:,:,2]
+
+print("shape I_x = ", I_x.shape)
+print("shape I_y = ", I_y.shape)
+# first index: i (x/col)
+# second index: j (y/row)
+
+
+Nx = I_x.shape[0] + 1
+Ny = I_x.shape[1] 
+print("Nx = ", Nx, ", Ny = ", Ny)
+
+def plot_flux():
+    plt.clf()
+    m = np.zeros((Nx - 1, Ny - 1))
+    for i in range(Nx - 1):
+        for j in range(Ny - 1):
+            m[i,j] = I_x[i, j] - I_x[i,j+1]  + I_y[i+1,j] - I_y[i,j]
+            
+    m /= 4
+    m = np.flip(m, axis=1)
+    m = np.swapaxes(m, 0, 1)
+
+    plt.imshow(m, aspect='equal', cmap='gray')
+    plt.colorbar(format="%.1f", label='flux')
+
+plot_flux()
+plt.show()
